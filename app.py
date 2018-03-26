086997e1726c787a43a159325be750e7a0ed957e
veera <veeraanudeep@gmail.com>
2018-03-26 23:24:08 +0530

    APIs done

--- app.py
+++ app.py
@@ -1,23 +1,73 @@
-from flask import Flask, render_template, request
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.heroku import Heroku
+from flask import Flask, render_template, request, jsonify, abort
+from models import db, Topics
+from flask_heroku import Heroku
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/reddit'
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/reddit'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
-db = SQLAlchemy(app)
-
-class Topic(db.Model):
-	__tablename__ = "topics"
-	id = db.Column(db.Integer, primary_key=True)
-	content = db.Column(db.String(255))
-	n_upvotes = db.Column(db.Integer, default=0)
-	n_downvotes = db.Column(db.Integer, default=0)
+db.init_app(app)
 
 @app.route("/")
 def main():
     return 'Hello World !'
 
+@app.route("/add-topic/", methods= ['POST'])
+def add_topic():
+	""""Adds a topic"""
+	error = None
+	if request.method == 'POST':
+		content = request.form['content']
+		if not content:
+			return "content cannot be Empty"
+		topic = Topics()
+		topic.content = content
+		db.session.add(topic)
+		db.session.commit()
+		return jsonify(data = content)
+
+
+
+@app.route("/topics/", methods=['GET'])
+def get_topics():
+	"""Returns 20 topics sorted by upvotes, descending"""
+	topics = [topic.json() for topic in Topics.query.order_by(Topics.n_upvotes.desc()).limit(20).all()]
+	return jsonify(data= topics)
+
+
+@app.route("/delete-topic/", methods=['POST'])
+def delete_topic():
+	"""Delte a topic by ID"""
+	id = request.form['id']
+	if not id:
+		return abort(400)
+	topic = Topics.query.filter_by(id=id).first()
+	db.session.delete(topic)
+	db.session.commit()
+	return "success"
+
+
+@app.route("/upvote/<int:topic_id>/", methods=['POST'])
+def upvote(topic_id):
+	"""Upvote function increments the upvote count of a topic"""
+	topic = Topics.query.filter_by(id=topic_id).first()
+	if not topic:
+		return abort(400)
+	topic.n_upvotes = topic.n_upvotes + 1
+	db.session.commit()
+	return jsonify(data=topic.json())
+
+@app.route("/downvote/<int:topic_id>/", methods=['POST'])
+def downvote(topic_id):
+	"""Downvote function increments the downvote count of a topic"""
+	topic = Topics.query.filter_by(id=topic_id).first()
+	if not topic:
+		return abort(400)
+	topic.n_downvotes = topic.n_downvotes + 1
+	db.session.commit()
+	return jsonify(data=topic.json())
+
+
 if __name__ == '__main__':
-    app.debug = True
     app.run()
